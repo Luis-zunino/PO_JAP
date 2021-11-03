30fe5e69ca0016974623795a5431a62ee5a27d7c
Luis Zunino <72168167+Luis-zunino@users.noreply.github.com>
2021-11-03 12:49:43 -0300

    index.html

--- ASSETS/JS/index.js
+++ ASSETS/JS/index.js
@@ -10,7 +10,6 @@ document.getElementById("aside").addEventListener("click", (e) => {
   let pagina1 = $(".paginaUno");
   let pagina2 = $(".paginaDos");
   let pagina3 = $(".paginaTres");
-  let pagina4 = $(".paginaCuatro");
   let r = e.target.classList;
 
   if (r.contains("pagina1")) {
@@ -19,39 +18,22 @@ document.getElementById("aside").addEventListener("click", (e) => {
     pagina1.addClass("mostrar");
     pagina2.addClass("ocultar");
     pagina3.addClass("ocultar");
-    pagina4.addClass("ocultar");
-
   } else if (r.contains("pagina2")) {
     removerClases();
 
     pagina1.addClass("ocultar");
     pagina2.addClass("mostrar");
     pagina3.addClass("ocultar");
-    pagina4.addClass("ocultar");
-
   } else if (r.contains("pagina3")) {
     removerClases();
 
     pagina1.addClass("ocultar");
     pagina2.addClass("ocultar");
     pagina3.addClass("mostrar");
-    pagina4.addClass("ocultar");
-
-  } else if (r.contains("pagina4")) {
-    removerClases();
-
-    pagina1.addClass("ocultar");
-    pagina2.addClass("ocultar");
-    pagina3.addClass("ocultar");
-    pagina4.addClass("mostrar");
-
   }
-
   function removerClases() {
     pagina1.removeClass("mostrar");
     pagina2.removeClass("mostrar");
     pagina3.removeClass("mostrar");
-    pagina4.removeClass("mostrar");
-
   }
 });
